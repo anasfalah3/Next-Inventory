9d83635ad3e58e142bd3280c70e4cdf7ee9989cc
anasfalah3 <anasfalah3@gmail.com>
2026-01-06 01:24:46 +0100

    Refactor styling for SignIn page and remove unused CSS variables

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function SignInPage(){
       return(
-            <div className="min-h-screen flex items-center justify-center">
-                  <div>
+            <div className="min-h-screen flex items-center justify-center bg-gradiant-to-b from-purple-50 to-purple-100">
+                  <div className="max-w-md w-full space-y-8">
                         <SignIn/>
                         <Link href="/">Back to home page</Link>
                   </div>
