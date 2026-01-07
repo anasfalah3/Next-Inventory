69a6867c5006b8a5f45a0cff227da1d6002278b0
anasfalah3 <anasfalah3@gmail.com>
2026-01-07 23:47:45 +0100

    Setup Sidebar component and integrate it into DashboardPage

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,3 @@
+export default function Sidebar({currentPath ="/dashboard"}: {currentPath?: string}) {
+      return <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen p-6 z-10">Dashboard</div>;
+}
