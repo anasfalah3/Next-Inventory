48b88cb096e111f551d542238d245e2d58ce5621
anasfalah3 <anasfalah3@gmail.com>
2026-01-12 23:41:51 +0100

    Implement user authentication and enhance DashboardPage layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,39 @@
+import { prisma } from "@/lib/prisma";
 import Sidebar from "../components/sidebar";
+import { getCurrentUser } from "@/lib/auth";
 
-export default function DashboardPage(){
+export default async function DashboardPage(){
+
+      const user = await getCurrentUser();
+      const userId = user.id;
+
+      const totalProducts = await prisma.product.count({where:{userId}});
+
+      const lowStock = await prisma.product.count({where:{userId}});
+      const recent = await prisma.product.findMany({
+            where:{userId},
+            orderBy:{createdAt:"desc"},
+            take:5,
+      })
+      console.log({totalProducts, lowStock, recent});
+
       return (
             <div className="min-h-screen bg-gray-50">
                   <Sidebar currentPath="/dashboard"/>
+                  <main className="ml-64 p-8">
+                        {/* Header */}
+                        <div className="mb-8">
+                              <div className="flex items-center justify-center">
+                                    <div>
+                                          <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                                          <p className="text-sm text-gray-500">Welcome Back! Here is an overviw of your inventory</p>
+                                    </div>
+                              </div>
+                        </div>
+
+
+                        {/* Key Metrices */}
+                  </main>
             </div>
       );
 }
